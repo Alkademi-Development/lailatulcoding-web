3cc4357818d3e36f23cc81a17b5ff430d89a0d48
alifanantomi <alifanantomi2@gmail.com>
2023-07-03 17:46:58 +0700

    feat: example pos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,32 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <PostList
+      v-for="edge in $page.allPost.edges"
+      :key="edge.node.id"
+      :post="edge.node"
+    />
   </Layout>
 </template>
 
+<page-query>
+query {
+  allPost {
+    edges {
+      node {
+        path
+        title
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import PostList from '../components/PostList';
 export default {
+  components: { PostList },
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'My blog'
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <div>
+      <h1>{{$page.post.title}}</h1>
+      <div v-html="$page.post.content"></div>
+    </div>
+  </Layout>
+</template>
+  
+<page-query>
+query Post ($path: String!) {
+  post (path: $path) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      $page: undefined
+    }
+  }
+})
+</script>
